e3ae956cfe7081e51e65cef452571f035004e49f
jizong <jizong.peng.1@etsmtl.net>
2020-02-15 11:30:51 -0500

    adding python formator

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
-# Code referenced from https://gist.github.com/gyglim/1f8dfb1b5c82627ae3efcfbbadb9f514
+# Code referenced from
+# https://gist.github.com/gyglim/1f8dfb1b5c82627ae3efcfbbadb9f514
 import tensorflow as tf
 import numpy as np
 import scipy.misc
@@ -10,14 +11,14 @@ except ImportError:
 
 
 class Logger(object):
-
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
         self.writer = tf.summary.FileWriter(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        summary = tf.Summary(
+            value=[tf.Summary.Value(tag=tag, simple_value=value)])
         self.writer.add_summary(summary, step)
 
     def image_summary(self, tag, images, step):
@@ -28,7 +29,7 @@ class Logger(object):
             # Write the image to a string
             try:
                 s = StringIO()
-            except:
+            except BaseException:
                 s = BytesIO()
             scipy.misc.toimage(img).save(s, format="png")
 
@@ -37,7 +38,8 @@ class Logger(object):
                                        height=img.shape[0],
                                        width=img.shape[1])
             # Create a Summary value
-            img_summaries.append(tf.Summary.Value(tag='%s/%d' % (tag, i), image=img_sum))
+            img_summaries.append(
+                tf.Summary.Value(tag='%s/%d' % (tag, i), image=img_sum))
 
         # Create and write Summary
         summary = tf.Summary(value=img_summaries)
@@ -55,7 +57,7 @@ class Logger(object):
         hist.max = float(np.max(values))
         hist.num = int(np.prod(values.shape))
         hist.sum = float(np.sum(values))
-        hist.sum_squares = float(np.sum(values ** 2))
+        hist.sum_squares = float(np.sum(values**2))
 
         # Drop the start of the first bin
         bin_edges = bin_edges[1:]
--- train_v2.py
+++ train_v2.py
@@ -7,21 +7,34 @@ from tqdm import tqdm
 # --------------------------CUDA check-----------------
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # -------------init Seg---------------
-model_S = DenseNet(num_init_features=32, growth_rate=16, block_config=(4, 4, 4, 4), drop_rate=0.2, num_classes=num_classes).to(device)
+model_S = DenseNet(num_init_features=32,
+                   growth_rate=16,
+                   block_config=(4, 4, 4, 4),
+                   drop_rate=0.2,
+                   num_classes=num_classes).to(device)
 # --------------Loss---------------------------
 criterion_S = nn.CrossEntropyLoss().cuda()
 # setup optimizer
-optimizer_S = optim.Adam(model_S.parameters(), lr=lr_S, weight_decay=6e-4, betas=(0.97, 0.999))
-scheduler_S = optim.lr_scheduler.StepLR(optimizer_S, step_size=step_size_S, gamma=0.1)
+optimizer_S = optim.Adam(model_S.parameters(),
+                         lr=lr_S,
+                         weight_decay=6e-4,
+                         betas=(0.97, 0.999))
+scheduler_S = optim.lr_scheduler.StepLR(optimizer_S,
+                                        step_size=step_size_S,
+                                        gamma=0.1)
 # --------------Start Training and Validation ---------------------------
 if __name__ == '__main__':
-    #-----------------------Training--------------------------------------
+    # -----------------------Training--------------------------------------
     mri_data_train = H5Dataset("./data_train_nocut", mode='train')
-    trainloader = dataloader.DataLoader(mri_data_train, batch_size=batch_train, shuffle=True)
+    trainloader = dataloader.DataLoader(mri_data_train,
+                                        batch_size=batch_train,
+                                        shuffle=True)
     mri_data_val = H5Dataset("./data_val_nocut", mode='val')
-    valloader = dataloader.DataLoader(mri_data_val, batch_size=1, shuffle=False)
+    valloader = dataloader.DataLoader(mri_data_val,
+                                      batch_size=1,
+                                      shuffle=False)
     print('Rate     | epoch  | Loss seg| DSC_val')
-    for epoch in range (num_epoch):
+    for epoch in range(num_epoch):
         scheduler_S.step(epoch)
         # zero the parameter gradients
         model_S.train()
@@ -29,10 +42,11 @@ if __name__ == '__main__':
             images, targets = data
             # Set mode cuda if it is enable, otherwise mode CPU
             images = images.to(device)
-            targets = targets.to(device)           
+            targets = targets.to(device)
             optimizer_S.zero_grad()
             outputs = model_S(images)
-            loss_seg = criterion_S(outputs, targets) #Crossentropy loss for Seg
+            # Crossentropy loss for Seg
+            loss_seg = criterion_S(outputs, targets)
             loss_seg.backward()
             optimizer_S.step()
 
@@ -63,12 +77,12 @@ if __name__ == '__main__':
                 targets_val = targets_val.data[0].cpu().numpy()
                 dsc = []
                 for i in range(1, num_classes):  # ignore Background 0
-                    if (np.sum(targets_val[targets_val==i])>0):
+                    if (np.sum(targets_val[targets_val == i]) > 0):
                         dsc_i = dice(predicted, targets_val, i)
                         dsc.append(dsc_i)
                 dsc = np.mean(dsc)
 
-        #-------------------Debug-------------------------
+        # -------------------Debug-------------------------
         # for param_group in optimizer_S.param_groups:
         #     print('%0.6f | %6d | %0.5f | %0.5f ' % (\
         #             param_group['lr'], epoch,
@@ -77,7 +91,9 @@ if __name__ == '__main__':
         #             #dsc for center path
         #             dsc))
 
-        #Save checkpoint
-        if (epoch % step_size_S) == 0 or epoch == (num_epoch - 1) or (epoch % 1000) == 0:
-            torch.save(model_S.state_dict(), './checkpoints/' + '%s_%s.pth' % (str(epoch).zfill(5), checkpoint_name))
-
+        # Save checkpoint
+        if (epoch % step_size_S) == 0 or epoch == (num_epoch -
+                                                   1) or (epoch % 1000) == 0:
+            torch.save(
+                model_S.state_dict(), './checkpoints/' + '%s_%s.pth' %
+                (str(epoch).zfill(5), checkpoint_name))
