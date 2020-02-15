ba6721534782fd99fc064b851676de067d6490ad
jizong <jizong.peng.1@etsmtl.net>
2020-02-15 17:08:25 -0500

    modifying the code to make it runable

--- train_v2.py
+++ train_v2.py
@@ -1,9 +1,10 @@
-import torch.utils.data as dataloader
-from dataloader import H5Dataset
 import torch.optim as optim
-from common import *
-from tqdm import tqdm
+import torch.utils.data as dataloader
 
+from common import *
+from dataloader import H5Dataset
+
+torch.backends.cudnn.benchmark = True
 # --------------------------CUDA check-----------------
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # -------------init Seg---------------
@@ -38,7 +39,7 @@ if __name__ == '__main__':
         scheduler_S.step(epoch)
         # zero the parameter gradients
         model_S.train()
-        for i, data in tqdm(enumerate(trainloader)):
+        for i, data in enumerate(trainloader):
             images, targets = data
             # Set mode cuda if it is enable, otherwise mode CPU
             images = images.to(device)
@@ -50,50 +51,48 @@ if __name__ == '__main__':
             loss_seg.backward()
             optimizer_S.step()
 
-        # -----------------------Validation------------------------------------
-        # no update parameter gradients during validation
-        with torch.no_grad():
-            for data_val in valloader:
-                images_val, targets_val = data_val
-                model_S.eval()
-                images_val = images_val.to(device)
-                targets_val = targets_val.to(device)
+            # -----------------------Validation------------------------------------
+            # no update parameter gradients during validation
+            with torch.no_grad():
+                for data_val in valloader:
+                    images_val, targets_val = data_val
+                    model_S.eval()
+                    images_val = images_val.to(device)
+                    targets_val = targets_val.to(device)
 
-                outputs_val = model_S(images_val)
-                _, predicted = torch.max(outputs_val.data, 1)
-                # ----------Compute dice-----------
-                predicted_val = predicted.data.cpu().numpy()
-                targets_val = targets_val.data.cpu().numpy()
-                dsc = []
-                for i in range(1, num_classes):  # ignore Background 0
-                    dsc_i = dice(predicted_val, targets_val, i)
-                    dsc.append(dsc_i)
-                dsc = np.mean(dsc)
-
-                outputs_val = model_S(images_val)
-                _, predicted = torch.max(outputs_val.data, 1)
-                # ----------Compute dice-----------
-                predicted = predicted.squeeze()
-                targets_val = targets_val.data[0].cpu().numpy()
-                dsc = []
-                for i in range(1, num_classes):  # ignore Background 0
-                    if (np.sum(targets_val[targets_val == i]) > 0):
-                        dsc_i = dice(predicted, targets_val, i)
+                    outputs_val = model_S(images_val)
+                    _, predicted = torch.max(outputs_val.data, 1)
+                    # ----------Compute dice-----------
+                    predicted_val = predicted.data.cpu().numpy()
+                    targets_val = targets_val.data.cpu().numpy()
+                    dsc = []
+                    for i in range(1, num_classes):  # ignore Background 0
+                        dsc_i = dice(predicted_val, targets_val, i)
                         dsc.append(dsc_i)
-                dsc = np.mean(dsc)
+                    dsc = np.mean(dsc)
 
-        # -------------------Debug-------------------------
-        # for param_group in optimizer_S.param_groups:
-        #     print('%0.6f | %6d | %0.5f | %0.5f ' % (\
-        #             param_group['lr'], epoch,
-        #             # loss_seg,
-        #             loss_seg.data.cpu().numpy(),
-        #             #dsc for center path
-        #             dsc))
+                    # outputs_val = model_S(images_val)
+                    # _, predicted = torch.max(outputs_val.data, 1)
+                    # # ----------Compute dice-----------
+                    # predicted = predicted.squeeze()
+                    # targets_val = targets_val.data[0].cpu().numpy()
+                    # dsc = []
+                    # for i in range(1, num_classes):  # ignore Background 0
+                    #     if (np.sum(targets_val[targets_val==i])>0):
+                    #         dsc_i = dice(predicted, targets_val, i)
+                    #         dsc.append(dsc_i)
+                    # dsc = np.mean(dsc)
 
-        # Save checkpoint
-        if (epoch % step_size_S) == 0 or epoch == (num_epoch -
-                                                   1) or (epoch % 1000) == 0:
-            torch.save(
-                model_S.state_dict(), './checkpoints/' + '%s_%s.pth' %
-                (str(epoch).zfill(5), checkpoint_name))
+            # -------------------Debug-------------------------
+            for param_group in optimizer_S.param_groups:
+                print('%0.6f | %6d | %0.5f | %0.5f ' % ( \
+                    param_group['lr'], epoch,
+                    # loss_seg,
+                    loss_seg.data.cpu().numpy(),
+                    # dsc for center path
+                    dsc))
+
+            # Save checkpoint
+            if (epoch % step_size_S) == 0 or epoch == (num_epoch - 1) or (epoch % 1000) == 0:
+                torch.save(model_S.state_dict(),
+                           './checkpoints/' + '%s_%s.pth' % (str(epoch).zfill(5), checkpoint_name))
